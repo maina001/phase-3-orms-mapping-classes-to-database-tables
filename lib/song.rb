4b3cf76eb79edd0feb15bb452c8afdf5bd30fb90
Brian <brianmakorious@gmail.com>
2023-02-22 14:25:45 +0300

    creating table

--- lib/song.rb
+++ lib/song.rb
@@ -8,3 +8,23 @@ class Song
   end
 
 end
+
+def self.drop_table
+  sql = <<-SQL
+    DROP TABLE IF EXISTS songs
+  SQL
+
+  DB[:conn].execute(sql)
+end
+
+ef self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+      )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
